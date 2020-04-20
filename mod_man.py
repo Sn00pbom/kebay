3dda081f793f42a544167adbfb9e72fde148ae88
Zach Lefin <zachlefin@gmail.com>
2020-04-20 18:51:36 -0400

    init module manager

---
+++ mod_man.py
@@ -0,0 +1,14 @@
+import os
+import json
+from importlib.machinery import SourceFileLoader
+
+
+def load_modules():
+    modules_path = './modules'
+    names = os.listdir(modules_path)
+    modules = {}
+    for name in names:
+        path = os.path.join(modules_path, name, 'launch.py')
+        modules[name] = SourceFileLoader(name, path)
+
+    return modules
