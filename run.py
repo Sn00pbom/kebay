0cd20803465a4d44cf227efd40721dcd21e0dc1c
Zach Lefin <zachlefin@gmail.com>
2020-04-20 21:36:20 -0400

    change run script to load modules

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import gui
+import mod_man
 
 if __name__ == "__main__":
-    gui.start()
+    modules, manifests = mod_man.load_modules()
+
+    # separate args sets from manifests
+    args_sets = {entry['name']: entry['args'] for entry in manifests.values()}
+
+    gui.start(modules, args_sets)
